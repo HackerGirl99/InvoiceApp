3643543a8c757837d037fddface95b72469b2575
HackerGirl99 <asnaniyas98@gmail.com>
2024-07-07 20:16:03 +0530

    removed changes that caused error

--- server/src/main/java/com/invoiceprocessing/server/dao/InvoiceDao.java
+++ server/src/main/java/com/invoiceprocessing/server/dao/InvoiceDao.java
@@ -1,10 +1,9 @@
+
 package com.invoiceprocessing.server.dao;
 
-import com.example.invoiceapp.model.User;
+import com.invoiceprocessing.server.model.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+public interface InvoiceDao extends JpaRepository<Invoice, Long> {
+
 }
