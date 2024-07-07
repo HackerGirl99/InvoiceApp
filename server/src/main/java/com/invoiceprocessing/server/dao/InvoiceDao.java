47430ede3e6aab3bf37ccf6ba8e3d1d2bfd8a5d3
HackerGirl99 <asnaniyas98@gmail.com>
2024-07-07 19:40:36 +0530

    Changes in server

--- server/src/main/java/com/invoiceprocessing/server/dao/InvoiceDao.java
+++ server/src/main/java/com/invoiceprocessing/server/dao/InvoiceDao.java
@@ -1,8 +1,10 @@
 package com.invoiceprocessing.server.dao;
 
-import com.invoiceprocessing.server.model.Invoice;
+import com.example.invoiceapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface InvoiceDao extends JpaRepository<Invoice, Long> {
-
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
