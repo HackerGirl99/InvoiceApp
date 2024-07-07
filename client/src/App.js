82df8dc2a654d0289daa46b0aa14bafaf5224501
HackerGirl99 <asnaniyas98@gmail.com>
2024-07-07 14:39:41 +0530

    Added Changes to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <Home />
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/aboutus" element={<AboutUs />} />
+                <Route path="/contactus" element={<ContactUs />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
